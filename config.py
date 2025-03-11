a71294abd07b64d88a3c059b21b41779fa6a7d08
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2025-03-11 13:30:29 -0400

    finished

--- config.py
+++ config.py
@@ -5,16 +5,21 @@
 # Pygame configuration Module
 
 WHITE = (255, 255, 255)
+GRAY = (128, 128, 128)
 BLACK = (0, 0, 0)
+
 RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
+CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
 
 # Window dimentions
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 
-# Window CAption
+# Window Caption
 
 TITLE = 'My Pygame Project'
 
--- main.py
+++ main.py
@@ -5,9 +5,11 @@
 import pygame
 import sys
 import config
+import random
 
 def init_game():
     pygame.init()
+    pygame.font.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
     pygame.display.set_caption(config.TITLE)
     return screen
@@ -24,14 +26,42 @@ def main():
     screen = init_game()
     clock = pygame.time.Clock()
     running = True
+
+    #  FONT
+    basic_fontname = 'FreeMono.ttf'
+    custom_fontname = 'PixelifySans-VariableFont_wght.ttf'
+    fsize_normal = 36
+    fsize_italic = 30
+    fsize_custom = 48
+
+    fpos1 = (random.randint(0, 400), random.randint(0, 300))
+    fpos2 = (random.randint(0, 400), random.randint(200, 500))
+    fpos3 = (random.randint(200, 600), random.randint(200, 500))
+
     while running:
         running = handle_events()
         screen.fill(config.WHITE)
-        pygame.display.flip()
 
-        # Limit clock to FPS
+        draw_text(screen, 'Regular Text', fsize_normal, config.BLACK, fpos1)
+        draw_text(screen, 'Oddish', fsize_italic, config.CYAN, fpos2, font_name=basic_fontname, bold=True, italic=True)
+        draw_text(screen, 'Abnormal', fsize_custom, config.PURPLE, fpos3, custom_fontname)
+
+        # Next frame
+        pygame.display.flip()
         clock.tick(config.FPS)
     pygame.quit()
     sys.exit()
+
+# Nonstandard Functions
+
+def draw_text(screen, text, font_size, color, position, font_name='FreeMono.ttf', antianiased=True, bold=False, italic=False):
+    font = pygame.font.Font(font_name, font_size)
+    font.set_bold(bold)
+    font.set_italic(italic)
+
+    textsurface = font.render(text, antianiased, color)
+    screen.blit(textsurface, position)
+
+
 if __name__ == '__main__':
     main()
